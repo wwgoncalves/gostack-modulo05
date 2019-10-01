53706797f7836ba4bf734abc90902d41a31713b2
lilo <williamcco@gmail.com>
2019-10-01 17:17:01 -0300

    fetching github repositories and stylizing

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { Component } from "react";
+import { FaGithubAlt, FaPlus, FaSpinner } from "react-icons/fa";
 
-import { Title } from "./styles";
+import api from "../../services/api";
 
-export default function Main() {
-  return <Title>Hello World</Title>;
+import { Container, Form, SubmitButton } from "./styles";
+
+export default class Main extends Component {
+  state = {
+    newRepository: "",
+    repositories: [],
+    loading: false,
+  };
+
+  handleInputChange = event => {
+    this.setState({ newRepository: event.target.value });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepository, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepository}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepository: "",
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepository, loading } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositories
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add repository"
+            value={newRepository}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#fff" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
